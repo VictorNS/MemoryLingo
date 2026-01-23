1debbab60bae9eddcddb89b5c35e50edbfe2fa4b
VictorNS <VictorNS@gmail.com>
2026-01-23 17:42:09 +0200

    Introduce space as a separate punctuation symbol

--- Core/Models/WordCheckResult.cs
+++ Core/Models/WordCheckResult.cs
@@ -2,7 +2,7 @@ namespace MemoryLingo.Core.Models;
 
 public record WordCheckResult(string Text, bool IsMatch, WordCheckResultType EntryType)
 {
-	public bool IsPunctuation => EntryType == WordCheckResultType.Punctuation;
+	public bool NoSpaceBefore => EntryType == WordCheckResultType.Punctuation;
 	public bool IsWord => EntryType == WordCheckResultType.Word;
 }
 
